5d892d6c0a6ecb404cba4bed4eaa85946d8f4bdf
BossDaily <comparatorcraft2020@gmail.com>
2022-04-20 20:24:09 -0500

    added boilerplate for an event that checks presences

---
+++ src/events/presenceUpdate/GameChecker.ts
@@ -0,0 +1,15 @@
+import { Message, MessageEmbed } from 'discord.js';
+
+import CommandSource from '../../structures/commands/CommandSource';
+import { Colors, Symbols } from '../../utils/Constants';
+import { sendTemporal, canMemberExecute, getMessageOptions } from '../../utils/DiscordUtils';
+import { getCommandName } from '../../utils/CommandUtils';
+import Event from '../../structures/Event';
+import MessageArgumentsParser from '../../MessageArgumentsParser';
+import { split } from '../../utils/StringUtils';
+
+export default class GameChecker extends Event {
+    override async run( ){
+
+    }
+}
