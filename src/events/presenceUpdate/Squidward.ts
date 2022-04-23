82993d3a19e57964ba243ba34df997315e68a398
BossDaily <comparatorcraft2020@gmail.com>
2022-04-23 09:36:58 -0500

    uses proper font

--- src/events/presenceUpdate/Squidward.ts
+++ src/events/presenceUpdate/Squidward.ts
@@ -5,7 +5,7 @@ import { MessageAttachment } from "discord.js";
 export const SquidWard = (vidya: string) => {
     const canvas = createCanvas(600, 548)
     const ctx = canvas.getContext('2d')
-    ctx.font = '48ptx impact'
+    ctx.font = '48px Impact'
     ctx.fillStyle = '#fffff'
     ctx.fillText(`${vidya} player`, canvas.width/ 1.5, canvas.height / 0.5)
     const bg = loadImage('https://media.discordapp.net/attachments/762378948566319136/967236176673128478/squidwardpointlaugh.jpg')
