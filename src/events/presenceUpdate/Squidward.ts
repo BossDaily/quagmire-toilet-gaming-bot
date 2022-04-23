34006053b13981528d1a4e1f0f0ffacd28884ce9
BossDaily <comparatorcraft2020@gmail.com>
2022-04-23 08:58:31 -0500

    changed variable names and loaded the background image

--- src/events/presenceUpdate/Squidward.ts
+++ src/events/presenceUpdate/Squidward.ts
@@ -2,6 +2,10 @@
 import { Canvas,createCanvas, loadImage } from "canvas";
 
 const SquidWard = (vidya: string) => {
-    const canvas = createCanvas(600, 548);
-    const context = canvas.getContext('2d');
+    const canvas = createCanvas(600, 548)
+    const ctx = canvas.getContext('2d')
+
+    const bg = loadImage('https://media.discordapp.net/attachments/762378948566319136/967236176673128478/squidwardpointlaugh.jpg')
+
+
 }
