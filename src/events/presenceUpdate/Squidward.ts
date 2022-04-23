40e752d9fee350852fb0671ab8a461b085a23d52
BossDaily <comparatorcraft2020@gmail.com>
2022-04-23 08:56:42 -0500

    proper canvas creation

--- src/events/presenceUpdate/Squidward.ts
+++ src/events/presenceUpdate/Squidward.ts
@@ -1,7 +1,7 @@
 
-import { Canvas } from "canvas";
+import { Canvas,createCanvas, loadImage } from "canvas";
 
 const SquidWard = (vidya: string) => {
-    const canvas = Canvas.createCanvas(600, 548);
+    const canvas = createCanvas(600, 548);
     const context = canvas.getContext('2d');
 }
