b9a6d6a9aeb1b0664ff7f898155e2a78dcc56459
BossDaily <comparatorcraft2020@gmail.com>
2022-04-23 09:23:59 -0500

    font context

--- src/events/presenceUpdate/Squidward.ts
+++ src/events/presenceUpdate/Squidward.ts
@@ -4,7 +4,9 @@ import { Canvas,createCanvas, loadImage } from "canvas";
 const SquidWard = (vidya: string) => {
     const canvas = createCanvas(600, 548)
     const ctx = canvas.getContext('2d')
-
+    ctx.font = '48ptx impact'
+    ctx.fillStyle = '#fffff'
+    
     const bg = loadImage('https://media.discordapp.net/attachments/762378948566319136/967236176673128478/squidwardpointlaugh.jpg')
 
 
