21755c82eead66aec4fc148d01e8abad4d1aa478
BossDaily <comparatorcraft2020@gmail.com>
2022-04-30 09:12:06 -0500

    changed text position and added a function that adjusts size for the text to make it fit

--- src/utils/Squidward.ts
+++ src/utils/Squidward.ts
@@ -2,19 +2,35 @@
 import { Canvas,createCanvas, loadImage } from "canvas";
 import { Channel, MessageAttachment } from "discord.js";
 
+const applyText = (canvas:Canvas, text:string) => {
+	const context = canvas.getContext('2d');
+
+	// Declare a base size of the font
+	let fontSize = 70;
+
+	do {
+		// Assign the font to the context and decrement it so it can be measured again
+		context.font = `${fontSize -= 10}px sans-serif`;
+		// Compare pixel width of the text to the canvas minus the approximate avatar size
+	} while (context.measureText(text).width > canvas.width - 300);
+
+	// Return the result to use in the actual canvas
+	return context.font;
+};
+
 export const SquidWard = async (vidya: string, channel:any, user: string) => {
     const canvas = createCanvas(600, 548)
     const ctx = canvas.getContext('2d')
 
-    ctx.font = '48px Impact'
-    ctx.fillStyle = '#ffffff'
-    ctx.fillText(`${vidya} player`, canvas.width/ 1.5, canvas.height / 0.5)
+    
 
     const bg = await loadImage('https://media.discordapp.net/attachments/762378948566319136/967236176673128478/squidwardpointlaugh.jpg')
     
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height)
 
-        
+    ctx.font = '48px impact'
+    ctx.fillStyle = '#ffffff'
+    ctx.fillText( `${vidya} player`, canvas.width / 5.2, canvas.height / 1.2 )    
 
         
     
