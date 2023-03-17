5b0570bf9eae7f91ef50ac8756cc503fbc3d3e6c
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-17 19:25:23 +0000

    comma

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const client = new SapphireClient({
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildVoiceStates,
-		GatewayIntentBits.MessageContent
+		GatewayIntentBits.MessageContent,
 	],
 	partials: [Partials.Channel],
 	loadMessageCommandListeners: true
