6b3da18210cda52b65a4b1527530a2a9b1df986f
BossDaily <gboys2008@gmail.com>
2023-05-10 15:31:28 +0000

    Mentioning the bot will query it

--- src/listeners/mentionPrefixOnly.ts
+++ src/listeners/mentionPrefixOnly.ts
@@ -1,10 +1,25 @@
-import type { Events } from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Events } from '@sapphire/framework';
 import { Listener } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
-export class UserEvent extends Listener<typeof Events.MentionPrefixOnly> {
+@ApplyOptions<Listener.Options>({ event: Events.MessageCreate })
+export class UserEvent extends Listener<typeof Events.MessageCreate> {
 	public async run(message: Message) {
-		const prefix = this.container.client.options.defaultPrefix;
-		return message.channel.send(prefix ? `My prefix in this guild is: \`${prefix}\`` : 'Cannot find any Prefix for Message Commands.');
+		const client = this.container.client;
+
+		if (message.content.includes(`<@${client.user?.id}>`)) {
+			const prompt = `You are talking to ${
+				message.author.username
+			}, here is what they said: "${message.content.replaceAll(`<@${client.user?.id}>`, '')}"`;
+			const promptEncoded = encodeURIComponent(prompt!);
+			const url = `${process.env.GPT_URL}ask?model=you&prompt=`;
+			await message.channel.sendTyping()
+			const promptGpt = await fetch(`${url}${promptEncoded}`).then();
+			
+			const res = await promptGpt.text();
+			
+			await message.reply(res);
+		}
 	}
 }
