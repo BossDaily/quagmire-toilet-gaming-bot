0d6dcebb86d3a859525fb747dedca7b45711692e
BossDaily <gboys2008@gmail.com>
2023-12-18 13:43:55 -0600

    rearanged preconditions

--- src/preconditions/voicecall.ts
+++ src/preconditions/voicecall.ts
@@ -6,10 +6,12 @@ export class UserPrecondition extends Precondition {
 		if (!interaction.inGuild()) return this.error({ message: `You can't use this command in DMs` });
 		const member = await interaction.guild?.members.fetch(interaction.targetId);
 		const initiator = await interaction.guild?.members.fetch(interaction.user.id);
+		if (!member?.voice?.channelId) return this.error({ message: `You are not in a voice channel` });
+		if (member?.user.bot) return this.error({ message: `You can't use this command on a bot` });
 		if (!member?.voice?.channelId) return this.error({ message: `This user is not in a voice channel` });
 		if (initiator?.voice.channelId !== member?.voice.channelId)
 			return this.error({ message: `You are not in the same voice channel as this user` });
-		if (member?.user.bot) return this.error({ message: `You can't mute bots` });
+		
 		return this.ok();
 	}
 }
