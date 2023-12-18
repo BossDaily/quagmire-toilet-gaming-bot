798e97f4517201ac394bd6e74e624482d9e3b2c5
BossDaily <gboys2008@gmail.com>
2023-12-18 13:54:53 -0600

    You can't do this command on me 🙅🏿‍♀️🙅🏿‍♀️

--- src/preconditions/voicecall.ts
+++ src/preconditions/voicecall.ts
@@ -6,6 +6,7 @@ export class UserPrecondition extends Precondition {
 		if (!interaction.inGuild()) return this.error({ message: `You can't use this command in DMs` });
 		const member = await interaction.guild?.members.fetch(interaction.targetId);
 		const initiator = await interaction.guild?.members.fetch(interaction.user.id);
+		if(member?.user.id == "274973338676494347") return this.error({ message: `https://c.tenor.com/0PqYCdwhHDQAAAAM/discord.gif` });
 		if (!member?.voice?.channelId) return this.error({ message: `You are not in a voice channel` });
 		if (member?.user.bot) return this.error({ message: `You can't use this command on a bot` });
 		if (!member?.voice?.channelId) return this.error({ message: `This user is not in a voice channel` });
