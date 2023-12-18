3443668326efd08feae347e190324dd9a2a428d4
BossDaily <gboys2008@gmail.com>
2023-12-18 14:58:20 -0600

    Add check to prevent user from using command on themselves

--- src/preconditions/voicecall.ts
+++ src/preconditions/voicecall.ts
@@ -8,6 +8,7 @@ export class UserPrecondition extends Precondition {
 		const initiator = await interaction.guild?.members.fetch(interaction.user.id);
 		if(member?.user.id == "274973338676494347") return this.error({ message: `https://c.tenor.com/0PqYCdwhHDQAAAAM/discord.gif` });
 		if (!member?.voice?.channelId) return this.error({ message: `You are not in a voice channel` });
+		if(member?.user.id == initiator?.user.id) return this.error({ message: `You can't use this command on yourself` });
 		if (member?.user.bot) return this.error({ message: `You can't use this command on a bot` });
 		if (!member?.voice?.channelId) return this.error({ message: `This user is not in a voice channel` });
 		if (initiator?.voice.channelId !== member?.voice.channelId)
