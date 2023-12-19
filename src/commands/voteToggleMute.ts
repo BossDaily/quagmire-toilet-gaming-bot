2855fb529197cebf54aef8e09747ffcb0bd05822
BossDaily <gboys2008@gmail.com>
2023-12-18 18:42:51 -0600

    added base embed

--- src/commands/voteToggleMute.ts
+++ src/commands/voteToggleMute.ts
@@ -1,7 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { BucketScope, Command } from '@sapphire/framework';
 import { Time } from '@sapphire/time-utilities';
-import { ActionRowBuilder, ApplicationCommandType, ButtonBuilder, ButtonStyle, MessageActionRowComponentBuilder } from 'discord.js';
+import { ActionRowBuilder, ApplicationCommandType, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageActionRowComponentBuilder } from 'discord.js';
 import { VoteObject } from '../lib/utils';
 
 @ApplyOptions<Command.Options>({
@@ -9,7 +9,7 @@ import { VoteObject } from '../lib/utils';
 	description: 'Initiates a vote to toggle mute a user in the voicechat',
 	preconditions: ['voicecall'],
 	cooldownDelay: Time.Second * 2,
-	cooldownScope: BucketScope.Guild,
+	cooldownScope: BucketScope.Guild
 })
 export class UserCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
@@ -38,27 +38,26 @@ export class UserCommand extends Command {
 		const muteString = isMuted ? 'unmute' : 'mute';
 		const votes: VoteObject[] = [];
 
-		const row = new ActionRowBuilder<MessageActionRowComponentBuilder>()
-         .addComponents(
-           new ButtonBuilder()
-             .setCustomId('vote_up')
-             .setLabel(`Vote to ${muteString}`)
-             .setStyle(ButtonStyle.Success)
-						 .setEmoji('👍'),
-           new ButtonBuilder()
-             .setCustomId('vote_down')
-             .setLabel(`Vote to not ${muteString}`)
-             .setStyle(ButtonStyle.Danger)
-						 .setEmoji('👎')
-         );
-		
+		const row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+			new ButtonBuilder().setCustomId('vote_up').setLabel(`Vote to ${muteString}`).setStyle(ButtonStyle.Success).setEmoji('👍'),
+			new ButtonBuilder().setCustomId('vote_down').setLabel(`Vote to not ${muteString}`).setStyle(ButtonStyle.Danger).setEmoji('👎')
+		);
 
-		await interaction.reply({ content: `You initiated a vote to ${muteString} <@${member?.user.id}>`, ephemeral:true });
-		const msg = await voiceChannel?.send({	
+		const voteEmbed = new EmbedBuilder()
+			.setColor('#2196f3')
+			.setThumbnail(`${member?.user.displayAvatarURL()}`)
+			.setTitle(`Vote to ${muteString} ${member?.user.displayName}`)
+			.setDescription(`<@${initiator?.user.id}> wants you to vote to mute <@${member?.user.id}> \n 
+			### The vote will end <t:${Math.floor(Time.Second * 45 / 1000)}:R>`
+			);
+			
+
+
+		await interaction.reply({ content: `You initiated a vote to ${muteString} <@${member?.user.id}>`, ephemeral: true });
+		const msg = await voiceChannel?.send({
 			content: `${vcMemberString}`,
-			components: [row]
+			components: [row],
+			embeds: [voteEmbed],
 		});
-
-		
 	}
 }
