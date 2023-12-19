67dbbbbfe7375d8c53a095e8b3a2dacabf802f0c
BossDaily <gboys2008@gmail.com>
2023-12-19 06:14:08 +0000

    updated collection update embed

--- src/commands/voteToggleMute.ts
+++ src/commands/voteToggleMute.ts
@@ -56,7 +56,7 @@ export class UserCommand extends Command {
 			.setThumbnail(`${member?.user.displayAvatarURL()}`)
 			.setTitle(`Vote to ${muteString} ${member?.user.displayName}`).setDescription(`<@${initiator?.user
 			.id}> wants you to vote to mute <@${member?.user.id}> \n 
-			### The vote will end <t:${Math.floor((Time.Second * 45) / 1000)}:R>`);
+			**The vote will end <t:${Math.floor((Date.now() + Time.Second * 45) / 1000)}:R>**`);
 
 		await interaction.reply({ content: `You initiated a vote to ${muteString} <@${member?.user.id}>`, ephemeral: true });
 		const msg = await voiceChannel?.send({
@@ -69,21 +69,23 @@ export class UserCommand extends Command {
 		collector?.on('collect', async (i) => {
 			if (i.user.id != initiator?.user.id) {
 				const vote = i.customId == 'vote_up' ? true : false;
+
 				const voteObject: VoteObject = {
 					user: await interaction.guild?.members.fetch(i.user.id),
 					vote: vote
 				};
 				if (votes.some((v) => v.user?.user.id == voteObject.user?.user.id)) {
-					votes.forEach((v) => {
+					votes.forEach( async (v) => {
 						if (v.user?.user.id == voteObject.user?.user.id) {
 							v.vote = voteObject.vote;
-							i.reply({ content: `You voted ${vote ? 'Yes' : 'No'} to ${muteString} <@${member?.user.id}>`, ephemeral: true });
+							await i.reply({ content: `You voted ${vote ? 'Yes' : 'No'} to ${muteString} <@${member?.user.id}>`, ephemeral: true });
 						}
 					});
 				} else {
 					votes.push(voteObject);
-					i.reply({ content: `You voted ${vote ? 'Yes' : 'No'} to ${muteString} <@${member?.user.id}>`, ephemeral: true });
+					await i.reply({ content: `You voted ${vote ? 'Yes' : 'No'} to ${muteString} <@${member?.user.id}>`, ephemeral: true });
 				}
+				
 				const yesVote = votes.filter((vote) => vote.vote == true);
 				const noVote = votes.filter((vote) => vote.vote == false);
 				const voteCountEmbed = new EmbedBuilder()
@@ -92,7 +94,7 @@ export class UserCommand extends Command {
 					.setTitle(`Vote to ${muteString} ${member?.user.displayName}`)
 					.setDescription(
 						`<@${initiator?.user.id}> wants you to vote to mute <@${member?.user.id}> \n 
-					### The vote will end <t:${Math.floor((Time.Second * 45) / 1000)}:R>`
+						**The vote will end <t:${Math.floor((Date.now() + Time.Second * 45) / 1000)}:R>**`
 					)
 					.addFields([
 						{ name: 'Voted to mute', value: `${yesVote.map((v) => `<@${v.user?.user.id}>`).join('\n')}`, inline: true },
