106e38be7e7a359acb2c083ef3cdb75fb360379c
BossDaily <gboys2008@gmail.com>
2025-06-13 17:17:44 -0500

    working drizzle integration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,14 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+
+// Load environment variables from .env file
+config({
+  path: './src/.env'
+});
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  schema: 'src/db/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 
 // You can specify any property from the libsql connection options
+// @ts-ignore
 const db = drizzle({ connection: { url: process.env.DB_FILE_NAME! }});
 
 const client = new SapphireClient({
